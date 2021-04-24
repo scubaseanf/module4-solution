2a667e67980ab91671b419dd79641f8d98e05b90
Sean Felts <34750464+scubaseanf@users.noreply.github.com>
2021-04-23 21:26:07 -0400

    Add files via upload

--- SpeakHello.js
+++ SpeakHello.js
@@ -13,7 +13,7 @@ var helloSpeaker = {};
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-  helloSpeaker.speak = function() {
+  helloSpeaker.speak = function(name) {
   console.log(helloSpeaker.speakWord + " " +  name);
 }
 
